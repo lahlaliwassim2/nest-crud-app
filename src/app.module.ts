99f573d36110d2f187992fdf1febcdf0c1a2902b
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-02 21:25:51 +0100

    connecter avec la base de donnée

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    UsersModule,
+  ],
 })
 export class AppModule {}
