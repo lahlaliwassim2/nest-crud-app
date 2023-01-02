8acd28b693fbd49faab618531ba96006eec6dc6a
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-02 23:28:02 +0100

    liaison entre userModule et la base de données

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/models/users.models';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
 })
