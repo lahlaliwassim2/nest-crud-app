23edbc58fa4766ab52264ea55ac044f7645d253b
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-02 21:16:25 +0100

    ajouter les controlleurs de mon projet

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,40 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Query,
+  Get,
+  Post,
+  Put,
+  Param,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly service: UsersService) {}
+
   @Post()
   Add() {
     return 'add actions';
+  }
+  @Get()
+  FindAll() {
+    return 'all users';
+  }
+  @Get('/:id')
+  FindOne(@Param('id') id: string) {
+    return id;
+  }
+  @Put('/:id')
+  Update(@Param('id') id: string) {
+    return id;
+  }
+  @Delete('/:id')
+  Delete(@Param('id') id: string) {
+    return id;
+  }
+  @Post('search')
+  Search(@Query('Key') Key) {
+    return Key;
   }
 }
