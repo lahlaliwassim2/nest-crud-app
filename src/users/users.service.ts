c7b57e2e199eaa27e69964741bd887c98ec75031
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-03 09:14:20 +0100

    ntegrer le model dans userService via un constructeur

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/models/users.models';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private catModel: Model<UserDocument>) {}
   Add() {
     return 'add user';
   }
