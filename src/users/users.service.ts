57bf08b483f081007296b3f13e2ee4d438e602a7
lahlaliwassim2 <wlahlali34@gmail.com>
2023-01-03 09:40:09 +0100

    completer la fonction Add User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UserDto } from 'src/dto/users.dto';
 import { User, UserDocument } from 'src/models/users.models';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private catModel: Model<UserDocument>) {}
-  Add() {
-    return 'add user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  Add(body : UserDto) {
+    return this.userModel.create(body);
   }
   FindAll() {
     return 'all users';
